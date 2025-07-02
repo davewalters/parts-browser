cf713e6f7e88df9418c31269eb01a2ad81eca5c0
dave.walters@designatwork.co.nz <dave.walters@designatwork.co.nz>
2025-07-02 08:01:04 +0000

    Created PartDetail and Form1.ItemTemplate1; Edited Form1; Deleted Form2

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,9 +1,32 @@
-from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.http
 
-class Form1(Form1Template):
+class Form1(Form1):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.repeating_panel_1.items = []
+    self.text_box_part_no.set_event_handler('change', self.update_filter)
+    self.text_box_desc.set_event_handler('change', self.update_filter)
+    self.update_filter()
 
-    # Any code you write here will run before the form opens.
+  def update_filter(self, **event_args):
+    prefix = self.text_box_part_no.text or ""
+    desc = self.text_box_desc.text or ""
+
+    try:
+      response = anvil.http.request(
+        url=f"http://127.0.0.1:8000/parts?prefix={prefix}",
+        method="GET",
+        json=True
+      )
+      filtered = [
+        part for part in response
+        if desc.lower() in (part.get("description") or "").lower()
+      ]
+      self.repeating_panel_1.items = filtered
+    except Exception as e:
+      self.label_status.text = f"❌ Error: {e}"
+      self.repeating_panel_1.items = []
+
+  def show_detail(self, part):
+    open_form("PartDetail", part=part)
---
+++ client_code/PartDetail/__init__.py
@@ -0,0 +1,13 @@
+from anvil import *
+
+class PartDetail(Form):
+  def __init__(self, part=None, **properties):
+    self.init_components(**properties)
+    self.label_id.text = part.get("_id", "")
+    self.label_rev.text = part.get("revision", "")
+    self.label_desc.text = part.get("description", "")
+    self.label_status.text = part.get("status", "")
+    self.label_vendor.text = part.get("default_vendor", "")
+
+  def button_back_click(self, **event_args):
+    open_form("Form1")
