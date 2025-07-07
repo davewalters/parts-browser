d97b4f8a8e83b94713984d8124e60e4fbcb88210
dave.walters@designatwork.co.nz <dave.walters@designatwork.co.nz>
2025-07-07 19:51:13 +0000

    Edited PartRecords and VendorRecords

--- client_code/PartRecords/__init__.py
+++ client_code/PartRecords/__init__.py
@@ -5,9 +5,10 @@ from ._anvil_designer import PartRecordsTemplate
 
 class PartRecords(PartRecordsTemplate):
   def __init__(self, filter_part="", filter_desc="", **kwargs):
+    #print(f"PartRecords.__init__ called with: filter_part={filter_part}, filter_desc={filter_desc}")
     self.init_components(**kwargs)
     self.button_new_part.role = "new-button"
-    self.button_vendors_list.role = "mydefault-button"
+    self.button_vendor_records.role = "mydefault-button"
 
     self.text_box_part_no.text = filter_part
     self.text_box_desc.text = filter_desc
@@ -22,7 +23,10 @@ class PartRecords(PartRecordsTemplate):
     self.text_box_desc.set_event_handler('change', self.update_filter)
     self.repeating_panel_1.set_event_handler("x-show-detail", self.show_detail)
 
-    self.update_filter()
+    try:
+      self.update_filter()
+    except Exception as e:
+      alert(f"update_filter failed: {e}")
 
   def update_filter(self, **event_args):
     prefix = self.text_box_part_no.text or ""
@@ -54,7 +58,7 @@ class PartRecords(PartRecordsTemplate):
               prev_filter_part=self.text_box_part_no.text,
               prev_filter_desc=self.text_box_desc.text)
 
-  def button_vendors_list_click(self, **event_args):
+  def button_vendor_records_click(self, **event_args):
     open_form("VendorRecords")
 
 
